371a99e17bdf764a017e17ed5e9202bcde793415
Mabeth <mmborres@gmail.com>
2019-05-17 02:35:18 +1000

    Updates

---
+++ precache-manifest.1b01dbd9074c7d3a4e6ec27c9401a77d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38ac6d5497f7d0761de9b5ec20d9c5e",
+    "url": "/pamper-my-pet/index.html"
+  },
+  {
+    "revision": "c0b19fd3e8f98b57ab40",
+    "url": "/pamper-my-pet/static/css/2.48f2b8f3.chunk.css"
+  },
+  {
+    "revision": "cb959e87a48bbce7e5da",
+    "url": "/pamper-my-pet/static/css/main.fe5f58af.chunk.css"
+  },
+  {
+    "revision": "c0b19fd3e8f98b57ab40",
+    "url": "/pamper-my-pet/static/js/2.1e39f7c4.chunk.js"
+  },
+  {
+    "revision": "cb959e87a48bbce7e5da",
+    "url": "/pamper-my-pet/static/js/main.0d9c4ef5.chunk.js"
+  },
+  {
+    "revision": "d939f1eba127bcfbe98c",
+    "url": "/pamper-my-pet/static/js/runtime~main.99c81498.js"
+  },
+  {
+    "revision": "f7f1f7ba1be24afdcbf77a3e3d928238",
+    "url": "/pamper-my-pet/static/media/Shopping-dog.f7f1f7ba.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pamper-my-pet/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pamper-my-pet/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pamper-my-pet/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pamper-my-pet/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pamper-my-pet/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
